c822022e842f737e831d50e357433c039db0a559
Anastasia Koschel <anastasia_koschel@teilnehmer.cdemy.com>
2025-10-14 08:22:16 +0200

    refactor: enhance documentation and structure for Liskov Substitution and Open/Close principles

--- lib/liskov_substitution/good_liskov.dart
+++ lib/liskov_substitution/good_liskov.dart
@@ -1,7 +1,16 @@
-abstract class Bird {}
+// Для того чтобы исправить нарушение принципа Лисков, мы можем создать иерархию классов, которая будет разделять птиц на
+// летающих и нелетающих. Таким образом, мы можем гарантировать, что все летающие птицы будут иметь метод fly, а нелетающие
+// птицы не будут.
+// При этом мы не нарушаем кон
+
+abstract class Bird {
+  void eat() => print('Bird is eating');
+}
 
 abstract class FlyingBird extends Bird {
   void fly();
+  @override
+  void eat() => print('Eat like a duck');
 }
 
 class Duck extends FlyingBird {
@@ -11,4 +20,6 @@ class Duck extends FlyingBird {
 
 class Penguin extends Bird {
   void swim() => print('Penguin is swimming');
+  @override
+  void eat() => print('Eat like a penguin');
 }
